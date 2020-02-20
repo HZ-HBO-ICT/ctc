637848a50af08ce23c2c2c1ad4c6f4262b3a21de
dwaard <daan.de.waard@hz.nl>
2020-02-20 12:38:21 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+![Logo of the project](https://avatars3.githubusercontent.com/u/40756580?s=200&v=4)
+
+# CasusToetsCopy (ctc)
+
+Copies all the relevant folders from one specific student repository folder to a base project folder
+and resets the app so that the students elaboration can be tested.
+
+## Features
+
+What's all the bells and whistles this project can perform?
+* Resets the project folder to HEAD in git
+* Copies app, database, resources and other relevant folders
+* Regenerates autoload files
+* Drop database tables and performs migration and seed
+* Clears compiled views
+
+## Installation
+
+Make sure the base repository of the casustoets project is correctly set up 
+with laradock and the laradock workspace is up.
+
+Copy the ctc.bat anywhere on your harddrive
+
+Create a .env file in the same path. You can use the .env.example file.
+
+Edit .env:
+* Set ProjectFolder to the base project folder
+* Set StudentsBaseFolder to the folder where all student repositories are 
+located
+* Set Laradock folder to the folder (relative to ProjectFolder) where 
+docker-compose commands can be executed
+* Set FileArray to a list of file and folder names in the root of the project 
+that should be copied
+
+
+## Usage
+
+Open a cmd in the folder where ctc.bat is located, and type:
+
+```shell script
+> ctc student-repo-folder-name
+```
+
+## Authors
+
+* **Daan de Waard** - *Initial work* - [dwaard](https://github.com/dwaard)
+
+See also the list of [contributors](https://github.com/HZ-HBO-ICT/blok-3-casustoets-bsmdb/graphs/contributors) who 
+participated in this project.
+
+## Licensing
+
+The code in this project is licensed under MIT license.
---
+++ ctc.bat
@@ -0,0 +1,96 @@
+@ECHO OFF
+
+REM ======================== READ CONFIG FROM .ENV ============================
+setlocal EnableDelayedExpansion
+FOR /f "delims=" %%x IN (.env) DO (
+  REM Set the loop param into a variable
+  SET "var=%%x"
+  REM Skip all lines that start with "#"
+  IF NOT "!var:~0,1!"=="#" (
+    REM Set the variable  
+    SET "!var!"
+  )
+)
+
+REM ========================= Validate user input =============================
+IF "%1"=="" GOTO HelpBasic
+
+REM Assign and validate student folder 
+SET StudentFolder=%StudentsBaseFolder%\%1
+IF NOT EXIST %StudentFolder% GOTO HelpFolderNotExist
+
+REM Remember the current folder
+SET StartFolder=%__CD__%
+
+REM Switch to the project folder
+CD %ProjectFolder%
+
+REM =================== Use GIT to reset the project ==========================
+ECHO Reset the base project folder to the HEAD version in git
+git reset --hard
+
+REM ================= Remove and copy files and folders =======================
+ECHO.
+ECHO Copy files and folders from %1
+FOR %%f IN (%FileArray%) DO (
+    SET "var=%%f"
+    ECHO !var! 
+
+    REM Set source and destination folders
+    CALL SET SourceFile=%StudentFolder%\!var!
+    CALL SET DestFile=!var!
+
+    REM Check whether SourceFile is a file or folder
+    IF EXIST !SourceFile!\* (
+        REM Clear old destination folder
+        IF EXIST !DestFile! (
+            RD /S /Q !DestFile!
+            MD !DestFile!
+        )
+        REM Copy from source to destination
+        XCOPY !SourceFile! !DestFile! /E /H /C /R /Q /Y
+    ) ELSE (
+        REM Remove old destination file
+        IF EXIST !DestFile! (
+            DEL !DestFile!
+        )
+        REM Copy from source to destination
+        COPY !SourceFile! !DestFile!
+    )
+)
+
+REM ======= Run some composer and artisan commands to reset the app ===========
+ECHO.
+CD %LaradockFolder%
+ECHO Update autoload file
+docker-compose exec workspace composer dump-autoload
+ECHO.
+ECHO Make fresh database and seed it
+docker-compose exec workspace php artisan migrate:fresh --seed
+ECHO.
+ECHO Clear all the compiled views
+docker-compose exec workspace php artisan view:clear
+
+REM ============ Change to the folder that was on the start of the ============
+CD %StartFolder%
+
+REM ============= Start Chrome browser with localhost URL =====================
+ECHO.
+ECHO Launching Chrome...
+START Chrome localhost 
+
+ECHO.
+ECHO Done! Enjoy Grading
+GOTO End
+
+REM ================ HELP MESSAGES ============================================
+:HelpBasic
+    ECHO Geen foldernaam aangegeven
+    ECHO usage: ctc valid-folder-name
+GOTO End
+
+:HelpFolderNotExist
+    ECHO %StudentFolder% bestaat niet
+
+REM ============================= END OF BATCH FILE ===========================
+:End
